bca49764d8613d61023757128b4c788b887c1ff3
Felipe Santos <felipe03san@gmail.com>
2023-05-26 17:08:15 -0300

    JS atualizado

--- js/script.js
+++ js/script.js
@@ -20,14 +20,49 @@
     //Event listeners
     form.addEventListener("submit", e => {
         e.preventDefault();
-
         if (!inputTask.value) return;
-
         const obj = createObject(inputTask.value);
-
-        
-
         renderLis(taskListArray);
+    })
+
+    ul.addEventListener("click", e => {
+        const el = e.target;
+        const li = el.parentElement;
+        const taskName = li.querySelector(".taskName").value;
+        const arrIndex = taskListArray.indexOf(taskListArray.filter(function (obj) {
+            return obj.name === taskName;
+        })[0]);
+
+        if (el.hasAttribute('btnActionRemove')) {
+            taskListArray.splice(arrIndex, 1);
+            el.parentElement.remove(li);
+        }
+
+        /*
+        if (el.hasAttribute('btnActionEdit')) {
+            if (li.querySelector(".taskName").disabled === true) {
+                li.querySelector(".taskName").disabled = false
+            } else {
+                li.querySelector(".taskName").disabled = true   
+            }
+            
+            taskListArray.forEach((element, index) => {
+                if (index === arrIndex){
+                    element.name = taskName;
+                }
+            });
+
+            //console.log(taskListArray[arrIndex])
+            //console.log(typeof taskName)
+            //console.log(taskListArray[arrIndex].name)
+            //taskListArray[arrIndex].name = taskName;
+        }
+        */
+
+        if (el.hasAttribute('btnActionCheck')) {
+            taskListArray[arrIndex].completed = !taskListArray[arrIndex].completed;
+            renderLis(taskListArray);
+        }
     })
     //Fim event listeners
 
@@ -46,18 +81,21 @@
         li.classList.add("taskLi");
         let checkBtn = document.createElement("i");
         checkBtn.setAttribute("class", "fa-sharp fa-solid fa-square icon")
+        checkBtn.setAttribute("btnActionCheck", "true")
         if (obj.completed) {
             checkBtn.classList.replace("fa-square", "fa-square-check");
-        } 
+        }
         let input = document.createElement("input");
         input.classList.add("taskName");
         input.setAttribute("type", "text");
-        input.setAttribute("disabled","true");
+        input.setAttribute("disabled", "true");
         input.value = obj.name;
         let editBtn = document.createElement("i");
         editBtn.setAttribute("class", "fa-sharp fa-solid fa-pen-to-square icon")
+        editBtn.setAttribute("btnActionEdit", "true")
         let remove = document.createElement("i");
         remove.setAttribute("class", "fa-solid fa-trash-can icon")
+        remove.setAttribute("btnActionRemove", "true")
 
         li.appendChild(checkBtn);
         li.appendChild(input);
@@ -67,7 +105,7 @@
         return li;
     };
 
-    function renderLis(arr){
+    function renderLis(arr) {
         ul.innerHTML = "";
         arr.forEach(element => {
             const li = createLi(element);
