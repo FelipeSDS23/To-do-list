2d5f424781765ae70d56434ed499ba42759f48e5
Felipe Santos <felipe03san@gmail.com>
2023-05-26 15:13:18 -0300

    JS atualizado

---
+++ js/script.js
@@ -0,0 +1,80 @@
+; (function () {
+    "use strict"
+
+    //Armazenamento do DOM em variáveis
+    const form = document.querySelector("#form");
+    const inputTask = document.querySelector("#inputTask");
+    const submitBtn = document.querySelector("#submitBtn");
+    const ul = document.querySelector("#taskList");
+    //Fim armazenamento do DOM em variáveis
+
+    //Obj lista
+    const taskListArray = [
+        {
+            name: "Teste1",
+            completed: true,
+        }
+    ]
+    //Fim Obj lista
+
+    //Event listeners
+    form.addEventListener("submit", e => {
+        e.preventDefault();
+
+        if (!inputTask.value) return;
+
+        const obj = createObject(inputTask.value);
+
+        
+
+        renderLis(taskListArray);
+    })
+    //Fim event listeners
+
+    //Funções
+    function createObject(name) {
+        let obj = {
+            name: name,
+            completed: false,
+        };
+        taskListArray.push(obj);
+        return obj;
+    };
+
+    function createLi(obj) {
+        let li = document.createElement("li");
+        li.classList.add("taskLi");
+        let checkBtn = document.createElement("i");
+        checkBtn.setAttribute("class", "fa-sharp fa-solid fa-square icon")
+        if (obj.completed) {
+            checkBtn.classList.replace("fa-square", "fa-square-check");
+        } 
+        let input = document.createElement("input");
+        input.classList.add("taskName");
+        input.setAttribute("type", "text");
+        input.setAttribute("disabled","true");
+        input.value = obj.name;
+        let editBtn = document.createElement("i");
+        editBtn.setAttribute("class", "fa-sharp fa-solid fa-pen-to-square icon")
+        let remove = document.createElement("i");
+        remove.setAttribute("class", "fa-solid fa-trash-can icon")
+
+        li.appendChild(checkBtn);
+        li.appendChild(input);
+        li.appendChild(editBtn);
+        li.appendChild(remove);
+
+        return li;
+    };
+
+    function renderLis(arr){
+        ul.innerHTML = "";
+        arr.forEach(element => {
+            const li = createLi(element);
+            ul.appendChild(li);
+        });
+    }
+    //Fim Funções
+
+
+})()
