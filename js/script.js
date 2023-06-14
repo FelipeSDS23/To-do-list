edd8da74f7f06701036af96e2c904e7b07c915d8
Felipe Santos <felipe03san@gmail.com>
2023-06-13 22:24:29 -0300

    Enviando todos os arquivos

--- js/script.js
+++ js/script.js
@@ -4,66 +4,60 @@
     //Armazenamento do DOM em variáveis
     const form = document.querySelector("#form");
     const inputTask = document.querySelector("#inputTask");
-    const submitBtn = document.querySelector("#submitBtn");
     const ul = document.querySelector("#taskList");
     //Fim armazenamento do DOM em variáveis
 
     //Obj lista
-    const taskListArray = [
-        {
-            name: "Teste1",
-            completed: true,
-        }
-    ]
+    const taskListArray = [{name: "Teste1", completed: true,}];
     //Fim Obj lista
 
     //Event listeners
-    form.addEventListener("submit", e => {
+    form.addEventListener("submit", (e) => {
         e.preventDefault();
         if (!inputTask.value) return;
         const obj = createObject(inputTask.value);
+        taskListArray.push(obj);
         renderLis(taskListArray);
-    })
+    });
+
 
     ul.addEventListener("click", e => {
         const el = e.target;
-        const li = el.parentElement;
-        const taskName = li.querySelector(".taskName").value;
-        const arrIndex = taskListArray.indexOf(taskListArray.filter(function (obj) {
-            return obj.name === taskName;
-        })[0]);
-
-        if (el.hasAttribute('btnActionRemove')) {
-            taskListArray.splice(arrIndex, 1);
-            el.parentElement.remove(li);
-        }
-
-        /*
-        if (el.hasAttribute('btnActionEdit')) {
-            if (li.querySelector(".taskName").disabled === true) {
-                li.querySelector(".taskName").disabled = false
-            } else {
-                li.querySelector(".taskName").disabled = true   
+        let li = el.parentElement;
+        
+        //Qualquer elemento clicado dentro da UL vai retornar no Dom até acha sua LI pai
+        if(el.nodeName !== "LI" && el.nodeName !== "P" && el.nodeName !== "UL"){
+            while(li.nodeName !== "LI") {
+                li = li.parentElement;
             }
-            
-            taskListArray.forEach((element, index) => {
-                if (index === arrIndex){
-                    element.name = taskName;
-                }
-            });
-
-            //console.log(taskListArray[arrIndex])
-            //console.log(typeof taskName)
-            //console.log(taskListArray[arrIndex].name)
-            //taskListArray[arrIndex].name = taskName;
         }
-        */
+        
+        let liList = ul.getElementsByTagName("li");
+        liList = Array.from(liList);
+
+        const arrIndex = liList.indexOf(li);
+
+        const div = li.querySelector("#editDiv");
 
         if (el.hasAttribute('btnActionCheck')) {
             taskListArray[arrIndex].completed = !taskListArray[arrIndex].completed;
             renderLis(taskListArray);
         }
+        if (el.hasAttribute('btnActionEdit')) {
+            div.setAttribute("class", "editDiv showEditDiv");
+        }
+        if(el.hasAttribute('btnActionConfirm')){
+            const editInput = li.querySelector(".editInput").value;
+            taskListArray[arrIndex].name = editInput;
+            renderLis(taskListArray);
+            div.setAttribute("class", "editDiv hideEditDiv");
+        }
+        if (el.hasAttribute('btnActionRemove')) {
+            taskListArray.splice(arrIndex, 1);
+            renderLis(taskListArray);
+        }
     })
+
     //Fim event listeners
 
     //Funções
@@ -72,35 +66,45 @@
             name: name,
             completed: false,
         };
-        taskListArray.push(obj);
         return obj;
     };
 
     function createLi(obj) {
-        let li = document.createElement("li");
+        const li = document.createElement("li");
         li.classList.add("taskLi");
-        let checkBtn = document.createElement("i");
-        checkBtn.setAttribute("class", "fa-sharp fa-solid fa-square icon")
-        checkBtn.setAttribute("btnActionCheck", "true")
+        const checkBtn = document.createElement("i");
+        checkBtn.setAttribute("class", "fa-sharp fa-solid fa-square icon");
+        checkBtn.setAttribute("btnActionCheck", "true");
         if (obj.completed) {
             checkBtn.classList.replace("fa-square", "fa-square-check");
         }
-        let input = document.createElement("input");
-        input.classList.add("taskName");
-        input.setAttribute("type", "text");
-        input.setAttribute("disabled", "true");
-        input.value = obj.name;
-        let editBtn = document.createElement("i");
-        editBtn.setAttribute("class", "fa-sharp fa-solid fa-pen-to-square icon")
-        editBtn.setAttribute("btnActionEdit", "true")
-        let remove = document.createElement("i");
-        remove.setAttribute("class", "fa-solid fa-trash-can icon")
-        remove.setAttribute("btnActionRemove", "true")
+        const paragraph = document.createElement("p");
+        paragraph.classList.add("taskName");
+        paragraph.textContent = obj.name;
+
+        const div = document.createElement("div"); 
+        div.setAttribute("class", "editDiv hideEditDiv");
+        div.setAttribute("id", "editDiv");
+        const input = document.createElement("input");
+        input.setAttribute("class", "editInput");
+        const confirmEditBtn = document.createElement("i"); 
+        confirmEditBtn.setAttribute("class", "fa-solid fa-check icon");
+        confirmEditBtn.setAttribute("btnActionConfirm", "true");
+        div.appendChild(input);
+        div.appendChild(confirmEditBtn);
+
+        const editBtn = document.createElement("i");
+        editBtn.setAttribute("class", "fa-sharp fa-solid fa-pen-to-square icon");
+        editBtn.setAttribute("btnActionEdit", "true");
+        const removeBtn = document.createElement("i");
+        removeBtn.setAttribute("class", "fa-solid fa-trash-can icon");
+        removeBtn.setAttribute("btnActionRemove", "true");
 
         li.appendChild(checkBtn);
-        li.appendChild(input);
+        li.appendChild(paragraph);
+        li.appendChild(div);
         li.appendChild(editBtn);
-        li.appendChild(remove);
+        li.appendChild(removeBtn);
 
         return li;
     };
@@ -113,6 +117,5 @@
         });
     }
     //Fim Funções
-
 
 })()
